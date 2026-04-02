236c5dfc3426dac47e3899deb4c4d984a5cb6d8a
Kostiantyn Yemelianov <k.yemelianov@gmail.com>
2026-04-02 14:01:33 +0200

    Replace println! with rclrs::log_info! for logging in rust_news_subscriber

--- src/my_rust_pkg/src/rust_news_subscriber.rs
+++ src/my_rust_pkg/src/rust_news_subscriber.rs
@@ -6,10 +6,11 @@ fn main() -> Result<()> {
     let context = Context::default_from_env()?;
     let mut _executor = context.create_basic_executor();
     let node = _executor.create_node("rust_news_subscriber")?;
-     println!("Created rust_news_subscriber node!");
+    rclrs::log_info!(node.logger(), "Created rust_news_subscriber node!");
 
-    let _subscriber = node.create_subscription("robot_news", |msg: String| {
-        println!("Received: {}", msg.data);
+    let log_node = node.clone();
+    let _subscriber = node.create_subscription("robot_news", move |msg: String| {
+        rclrs::log_info!(log_node.logger(), "Received: {}", msg.data);
     })?;
 
     _executor.spin(SpinOptions::default()).first_error()?;
